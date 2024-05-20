8b5799844bbed7682b7a0f1dab9a72d803ccccee
AimloQ <143868721+AimloQ@users.noreply.github.com>
2024-05-20 19:12:44 +0530

    Main.js---Complete setup function and make got result function and model loaded function..................Update Cooked

--- main.js
+++ main.js
@@ -3,7 +3,42 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier=ml5.imageClassifier('MobileNet',modelLoaded);
 }
 
+function modelLoaded()
+{
+  console.log("Cooking up yhe model");
+  console.log("Model Loaded!!!");
+}
 
+function draw()
+{
+  image(video,0,0,300,300);
+  classifier.classify(video,gotResult);
+}
 
+var previous_result='';
+
+function gotResult()
+{
+  if(error)
+    {
+      console.error(error);
+    }
+
+    else
+    {
+      if((results[0].confidence > 0.5) && (previous_result !=results[0].label))
+        {
+          console.log(results);
+          previous_result=results[0].label;
+          var synth=window.speechSynthesis;
+          speak_data='Object Detected is - '+results[0].label;
+          var utterThis= new SpeechSynthesisUtterance(speak_data);
+
+          document.getElementById("result_object_name").innerHTML=results[0].label;
+          document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+        }
+    }
+}
